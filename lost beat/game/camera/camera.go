8a0266e31260c64282dd23f1b0a7ef54ac397fdb
tlsqltnwls <tlsqltnwls@naver.com>
2022-02-09 01:06:27 +0900

    camera추가 / 기본 resource 파일 추가 / player sprite & 간단한 움직임 추가

---
+++ lost beat/game/camera/camera.go
@@ -0,0 +1,15 @@
+components {
+  id: "camera"
+  component: "/rendercam/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
