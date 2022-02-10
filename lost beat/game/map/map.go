19c9014f8ebaa013fc4b378b0078174cd4b6f960
ihaha424 <70957529+ihaha424@users.noreply.github.com>
2022-02-10 15:02:51 +0900

    임시

---
+++ lost beat/game/map/map.go
@@ -0,0 +1,50 @@
+components {
+  id: "map"
+  component: "/game/map/map.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "tilemap"
+  component: "/game/map/map.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/game/map/map.tilesource\"\n"
+  "default_animation: \"tile\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 26.0
+    y: -344.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
